46a24379172e719c08b885caae9de155164d6fe3
aliasgoeshere <ciscoderp@gmail.com>
2021-03-02 22:10:48 -0600

    Update member-counter.js

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
-    const guild = client.guilds.cache.get('810942568308146186');
+    const guild = client.guilds.cache.get('GUILD ID HERE');
     let FgRed = "\x1b[31m";
     let FgGreen = "\x1b[32m";
     let Reset = "\x1b[0m";
@@ -8,7 +8,7 @@ module.exports = async (client) => {
     setInterval(() => {
         const memberCount = guild.memberCount;
 
-        const channel = guild.channels.cache.get('810962591182094346');
+        const channel = guild.channels.cache.get('CHANNEL ID HERE');
         channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
         console.log(`${FgRed}Member Count is now: ${FgGreen}${memberCount.toLocaleString()} ${Reset}`);
     }, 600000);
